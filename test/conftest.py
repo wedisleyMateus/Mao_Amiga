6df5cc3601a37c252cf685ce00dc7e7374f191be
wedisleyMateus <wedmat282016@gmail.com>
2025-09-29 17:12:23 -0300

    test: configurando o conftest.py e fazendo teste de CRUD

--- test/conftest.py
+++ test/conftest.py
@@ -1,1 +1,34 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, String, Integer, Numeric
 
+DATABASE_TEST_URL = "sqlite+pysqlite:///:memory:"
+
+engine = create_engine(DATABASE_TEST_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+
+
+class Service(Base):
+    __tablename__ = "type_service"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    service_value = Column(Numeric(10, 2), nullable=False)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def criando_test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture
+def db():
+    session = TestingSessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
