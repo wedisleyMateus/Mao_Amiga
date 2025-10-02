c4b5098a1f3f7265d2be13a4f749a3dcc42c619b
wedisleyMateus <wedmat282016@gmail.com>
2025-10-02 13:18:27 -0300

    test: conflito de serviço existente

--- test/test_unit/service__api_test.py
+++ test/test_unit/service__api_test.py
@@ -1,18 +1,39 @@
 import pytest
 from app.api.service_api import create_service
+from fastapi import HTTPException
 from app.schemas.service_schema import ServiceVerificationSchema
 
 
 @pytest.mark.asyncio
 async def test_create_service(mocker):
-    mock_data = ServiceVerificationSchema(name="Salve jorge", service_value=120.2)
-
+    data = ServiceVerificationSchema(name="Pintura Acrilica", service_value=120.2)
     mock_db = mocker.Mock()
 
     mock_service_layer = mocker.Mock()
-    mock_service_layer.existence_verification.return_value = mock_data
+    mock_service_layer.existence_verification.return_value = data
 
     mocker.patch("app.api.service_api.ServiceLayer", return_value=mock_service_layer)
 
-    resultado = await create_service(mock_data, mock_db)
-    assert resultado.name == "Salve jorge"
+    resultado = await create_service(data, mock_db)
+    assert resultado.name == "Pintura Acrilica"
+    assert resultado.service_value == 120.2
+    mock_service_layer.existence_verification.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_create_service_existing(mocker):
+    data = mocker.Mock()
+    mock_db = mocker.Mock()
+
+    mock_service_layer = mocker.Mock()
+    mock_service_layer.existence_verification.side_effect = HTTPException(
+        status_code=409, detail="Serviço já existente"
+    )
+
+    mocker.patch("app.api.service_api.ServiceLayer", return_value=mock_service_layer)
+
+    with pytest.raises(HTTPException) as excecao:
+        await create_service(data, mock_db)
+
+    assert excecao.value.status_code == 409
+    assert excecao.value.detail == "Serviço já existente"
