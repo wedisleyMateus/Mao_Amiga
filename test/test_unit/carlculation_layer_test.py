91a3cae3a443178b6b18bbd4d02ab75ec5d75aa8
wedisleyMateus <wedmat282016@gmail.com>
2025-10-03 14:36:46 -0300

    test: adicionando test de exceção para teste unitario da função de squared_calculation

--- test/test_unit/carlculation_layer_test.py
+++ test/test_unit/carlculation_layer_test.py
@@ -1,4 +1,6 @@
+import pytest
 from decimal import Decimal
+from fastapi import HTTPException, status
 from app.repositories.service_repository import VerificationWithName
 from app.schemas.service_schema import ServiceCalculationSchema
 from app.service_layer.calculation_layer import squared_calculation
@@ -18,3 +20,20 @@ def test_calculation(mocker):
     assert resultado.name == "Pintura Acrilica"
     assert resultado.service_value == 120.5
     assert resultado.total == 2410.0
+
+
+def test_calculation_exception(mocker):
+    mock_data = mocker.Mock()
+    mock_data.name = ""
+
+    mock_verification = mocker.Mock(spec=VerificationWithName)
+    mock_verification.service_verification.side_effect = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Serviço '{mock_data.name}' não encontrado",
+    )
+
+    with pytest.raises(HTTPException) as excecao:
+        squared_calculation(mock_data, mock_verification)
+
+    assert excecao.value.status_code == status.HTTP_404_NOT_FOUND
+    assert excecao.value.detail == "Serviço '' não encontrado"
