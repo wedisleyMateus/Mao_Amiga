10ffdfd953f964ed433249647f5ac11131e8b65c
wedisleyMateus <wedmat282016@gmail.com>
2025-10-10 09:43:42 -0300

    refactor: separando responsabilidade do repositorio

--- app/repositories/service_repository.py
+++ app/repositories/service_repository.py
@@ -4,24 +4,23 @@ from app.models.service_model import Service
 from app.schemas.service_schema import ServiceSchema
 
 
-class VerificationInterface(ABC):
+class BaseService(ABC):
     def __init__(self, db: Session):
         self.db = db
 
     @abstractmethod
-    def service_verification(self, data):
+    def query_service(self, data):
         pass
 
 
-class ServiceVerificationByName(VerificationInterface):
-    def service_verification(self, data):
+class ServiceBase(BaseService):
+    def query_service(self, data):
         return self.db.query(Service).filter(Service.name == data).first()
 
 
-class ServiceRepository:
-    def __init__(self, db: Session, verification: VerificationInterface):
+class CreateService:
+    def __init__(self, db: Session):
         self.db = db
-        self.verification = verification
 
     def create_service(self, data):
         type_service = Service(name=data.name, value=data.service_value)
@@ -30,23 +29,37 @@ class ServiceRepository:
         self.db.refresh(type_service)
         return ServiceSchema.model_validate(type_service)
 
-    def get_all_service(self):
+
+class GetAllServices:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all_services(self):
         all_services = self.db.query(Service).all()
         return all_services
 
+
+class GetService(ServiceBase):
+
     def get_service(self, service_name):
-        service = self.verification.service_verification(service_name)
+        service = self.query_service(service_name)
         return service
 
+
+class UpdateService(ServiceBase):
+
     def update_service(self, service_name, service):
-        get_service = self.verification.service_verification(service_name)
+        get_service = self.query_service(service_name)
         get_service.name = service.name
         get_service.service_value = service.service_value
         self.db.commit()
         self.db.refresh(get_service)
         return get_service
 
+
+class DeleteService(ServiceBase):
+
     def delete_service(self, service_name):
-        get_service = self.verification.service_verification(service_name)
+        get_service = self.query_service(service_name)
         self.db.delete(get_service)
         self.db.commit()
