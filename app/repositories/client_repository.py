1ea80e1849dd7c603af26fe65602190b60f30cfd
wedisleyMateus <wedmat282016@gmail.com>
2025-10-14 16:58:19 -0300

    feat(rository): aplicar srp separando as operações crud

--- app/repositories/client_repository.py
+++ app/repositories/client_repository.py
@@ -1,41 +1,63 @@
 from app.models.client_model import Clients
-from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 from app.core.logger_config import logger
+from app.schemas.client_schema import ClientRead
 
 
-class ClientRepository:
+class RepositoryBase:
     def __init__(self, db: Session):
         self.db = db
 
 
-    def create(self, client: Clients) -> Clients:
+class RepositoryCreate(RepositoryBase):
+    def create(self, data) -> ClientRead:
+        client = Clients(
+            name=data.name,
+            email=data.email,
+            telephone=data.telephone,
+            address=data.address
+        )
         logger.info("Starting client creation process")
         self.db.add(client)
         self.db.commit()
         self.db.refresh(client)
         logger.info(f"Client {client.name} successfully created")
+        return ClientRead.model_validate(client)
+
+
+class RepositoryRetrieveByName(RepositoryBase):
+    def get_by_name(self, name: str) -> Clients | None:
+        logger.info("starting the process of finding a client")
+        client = self.db.query(Clients).filter(Clients.name == name).first()
+        logger.info(f"Found client {name} with id {client.id}")
         return client
 
 
-    def get_by_name(self, name: str) -> Clients:
-        logger.info("starting the process of finding a client")
-        result = self.db.execute(select(Clients).filter(Clients.name == name))
-        logger.info(f"Found client {name} with id {result.scalar()}")
-        return result.scalar_one_or_none()
-
-
-    def update(self, client: Clients) -> Clients:
+class RepositoryUpdate(RepositoryBase):
+    def update(self, client, data) -> ClientRead:
+        client.name = data.name
+        client.email = data.email
+        client.telephone = data.telephone
+        client.address = data.address
         logger.info(f"Starting update process for client id={client.id}")
         self.db.commit()
         self.db.refresh(client)
         logger.info(f"Client id={client.id} successfully updated")
-        return client
+        return ClientRead.model_validate(client)
 
 
-    def delete(self, client: Clients) -> None:
+class RepositoryDelete(RepositoryBase):
+    def delete(self, client)-> None:
         logger.info(f"Starting delete process for client id={client.id}")
         self.db.delete(client)
         self.db.commit()
         logger.indo("Delete with successfully deleted")
 
+
+class RepositoryCRUD(
+    RepositoryCreate,
+    RepositoryRetrieveByName,
+    RepositoryUpdate,
+    RepositoryDelete
+):
+    pass
