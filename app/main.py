12fce0d45fe4069a8b26257862f2568aa9e60382
wedisleyMateus <wedmat282016@gmail.com>
2025-10-15 23:04:13 -0300

    feat: definindo padrão nos nomes das class e tabelas

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.api import client_api, login_api, service_api, calculation
+from app.api import client_api, user_api, service_api, calculation_api
 
 app = FastAPI()
 
 app.include_router(service_api.router)
 app.include_router(client_api.router)
-app.include_router(login_api.router)
-app.include_router(calculation.router)
+app.include_router(user_api.router)
+app.include_router(calculation_api.router)
--- app/models/calculation_model.py
+++ app/models/calculation_model.py
@@ -1,15 +1,17 @@
 from sqlalchemy import Column, ForeignKey, Integer, Numeric
+from sqlalchemy.orm import relationship
 
 from app.infrastructure.conection import Base
 
-class CalculationRecord(Base):
-    __tablename__ = 'calculation_record'
+class Calculation(Base):
+    __tablename__ = 'calculation'
     id = Column(Integer, primary_key=True, autoincrement=True)
     service_id = Column(Integer, ForeignKey('service.id'))
-    client_id = Column(Integer, ForeignKey('clients.id'))
+    client_id = Column(Integer, ForeignKey('client.id'))
     value = Column(Numeric(10, 2), nullable=False)
     squad_value = Column(Numeric(10, 2), nullable=False)
     total = Column(Numeric(10, 2), nullable=False)
 
-    client = relationship('Clients', backref='calculations')
+    client = relationship('Client', backref='calculations')
+    service = relationship('Service', backref='calculations')
 
