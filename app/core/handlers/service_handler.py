471251f61f8df1ed0ffcc4e70b7e0c345f56d703
wedisleyMateus <wedmat282016@gmail.com>
2025-10-17 09:57:14 -0300

    refactor: criando um handler base para respeitar o DRY

--- app/core/handlers/service_handler.py
+++ app/core/handlers/service_handler.py
@@ -2,45 +2,28 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from app.core.logger_config import logger
+from app.core.handlers.base_handler import generic_error_handler
 from app.core.exceptions.service import (
     ServiceAlreadyExistsError,
     ServiceNotFoundError,
     ServiceListEmptyError
 )
 
-
-def generic_service_error_handler(
-        request,
-        exc,
-        exception_class,
-        status_code
-) -> JSONResponse:
-    if isinstance(exc, exception_class):
-        return JSONResponse(
-            status_code=status_code,
-            content={"message": exc.message_suffix}
-        )
-    return JSONResponse(
-        status_code=500,
-        content={"detail": "Internal Server Error"}
-    )
-
-
-def service_already_exists_handler(request, exc):
+def service_already_exists_handler(request: Request, exc: Exception) -> JSONResponse:
     logger.warning(f"ServiceAlreadyExistsError, path: {request.url.path}")
-    return generic_service_error_handler(
+    return generic_error_handler(
         request, exc, ServiceAlreadyExistsError, 402
     )
 
 
-def service_not_found_handler(request: Request, exc: Exception):
+def service_not_found_handler(request: Request, exc: Exception) -> JSONResponse:
     logger.warning(f"ServiceNotFoundError, path: {request.url.path}")
-    return generic_service_error_handler(
+    return generic_error_handler(
         request, exc, ServiceNotFoundError, 402
     )
 
 
-def service_list_handler(request: Request, exc: Exception):
+def service_list_handler(request: Request, exc: Exception) -> JSONResponse:
     if isinstance(exc, ServiceListEmptyError):
         logger.warning(f"ServiceListEmptyError, path: {request.url.path}")
         return JSONResponse(
